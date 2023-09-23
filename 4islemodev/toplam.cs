3eb46c5ad8182a2e6f499e4efd11c23fb791a704
BNilay <144630814+BNilay@users.noreply.github.com>
2023-09-23 16:58:16 +0300

    Add files via upload

---
+++ 4islemodev/toplam.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4islemodev
+{
+    internal class toplam
+    {
+        public int Toplam(int s1, int s2)
+        {
+            int sonuc = s1 + s2;
+            Console.WriteLine("sonuc: "+ sonuc );
+            return sonuc; 
+
+
+
+
+            
+        }
+
+        public int Fark(int s1, int s2)
+        {
+            int sonuc = s1 - s2;
+            Console.WriteLine("sonuc: "+sonuc );
+            return sonuc;
+        }
+
+        public int Carpım(int s1, int s2)
+        {
+            int sonuc = s1 * s2;
+            Console.WriteLine("sonuc: " + sonuc);
+
+            return sonuc;
+        }
+
+        public int Bolum(int s1, int s2)
+        {
+            int sonuc = s1 / s2;
+            Console.WriteLine("sonuc: "+ sonuc);
+            return sonuc;
+        }
+
+
+    }
+}
